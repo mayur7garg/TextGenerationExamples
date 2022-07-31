a4739c8f80bccbbc2ff6809f1d781bad8179819a
Mayur Garg <mayur7garg@gmail.com>
2022-07-31 12:12:33 +0000

    Restructured code. Added utility file

---
+++ Example_01/model_utility.py
@@ -0,0 +1,24 @@
+import re
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+
+def read_book_data(book_dir: Path, file_pat: str = "*.txt", seq_len: int = 512):
+    X = []
+    y = []
+
+    for book_file_path in book_dir.rglob(file_pat):
+        with book_file_path.open('r', encoding = 'utf-8') as book_file:
+            book_data = book_file.read()
+            book_data = re.sub("\n", " ", book_data)
+            book_data = re.sub("[ ]+", " ", book_data)
+            char_len = len(book_data)
+
+            for i in range(0, char_len - seq_len):
+                X.append(book_data[i : i + seq_len])
+                y.append(book_data[i + seq_len])
+
+    return X, y
+
+def get_train_val_data(book_dir: Path, file_pat: str = "*.txt", seq_len: int = 512, val_size: float = 0.05, random_state: int = 7):
+    X, y = read_book_data(book_dir, file_pat, seq_len)
+    return train_test_split(X, y, test_size = val_size, random_state = random_state)
